6ba77b393b642056962edcbd6685534bfbf71dfd
LukasJarmalavicius <lukisjarm@gmail.com>
2023-11-28 22:07:32 +0200

    intergrated the html with the code

--- script.js
+++ script.js
@@ -1,33 +1,105 @@
-function add(firstNumber, secondNumber){
-    return firstNumber + secondNumber;
-};
+let displayValue = '0'
+let firstNumber = null;
+let currentOperation = null;
+let secondNumber = null;
+let results = null;
+const topScreen = document.querySelector('.topScreen')
+const botScreen = document.querySelector('.botScreen')
+botScreen.textContent = displayValue;
+// Click addEventListener
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(button.className === 'operand'){
+            addOperand(button.id);
+            botScreen.textContent = displayValue;
+        }else if(button.className === 'operator'){
+            addOperator(button.id);
+            botScreen.textContent = displayValue;
+        }else if(button.id === '='){
+            equals()
+        }else if(button.id === 'clearbtn'){
+            clearScreen()
+        }
+    })
+})
 
-function subtract(firstNumber, secondNumber){
-    return firstNumber - secondNumber;
-};
 
-function multiply(firstNumber, secondNumber){
-    return firstNumber * secondNumber;
-};
+//Calculator Functions
+function addOperand(operand){
+    if(currentOperation === null){
+        if(firstNumber === null){
+            firstNumber = operand;
+           displayValue = firstNumber;
+        }
+        else{
+            firstNumber += operand;
+            displayValue = firstNumber;
+        }
 
-function divide(firstNumber, secondNumber){
-    return firstNumber / secondNumber;
-};
+    }else{
+        if(secondNumber === null){
+            secondNumber = operand;
+            displayValue = secondNumber;
+        }
+        else{
+            secondNumber += operand;
+            displayValue = secondNumber;
+        }
+    }
+    
+}
 
-let firstNumber;
-let operator;
-let secondNumber;
+function addOperator(operator){
+    if(currentOperation === null){
+        currentOperation = operator;
+        topScreen.textContent = displayValue + operator
+        botScreen.textContent = displayValue;
+    } else if(currentOperation != null){
+        results = Math.round(operate(firstNumber, currentOperation, secondNumber))
+        currentOperation = operator
+        firstNumber = results;
+        secondNumber = null
+        displayValue = results;
+        results = null;
+
+        topScreen.textContent = displayValue + operator
+        botScreen.textContent = displayValue;
+     
+    }
+   
+
+}
+
+function equals(){
+    results = operate(firstNumber, currentOperation, secondNumber)
+    displayValue = results;
+    results = null;
+    currentOperation = null;
+    botScreen.textContent = displayValue;
+
+}
 
 function operate(firstNumber, operator, secondNumber){
-    operator = operator.toLowerCase()
+    firstNumber = parseInt(firstNumber)
+    secondNumber = parseInt(secondNumber)
     switch(operator){
-        case 'add':
-            return add(firstNumber, secondNumber);
-        case `subtract`:
-            return subtract(firstNumber, secondNumber);
-        case 'multiply':
-            return multiply(firstNumber, secondNumber);
-        case 'divide':
-            return divide(firstNumber, secondNumber);    
+        case '+':
+            return firstNumber + secondNumber;
+        case `-`:
+            return firstNumber - secondNumber;
+        case '*':
+            return firstNumber * secondNumber;
+        case '÷':
+            return firstNumber / secondNumber;    
     }
+}
+
+function clearScreen(){
+    firstNumber = null;
+    secondNumber = null;
+    currentOperation = null;
+    displayValue = '0';
+    topScreen.textContent = ''
+    botScreen.textContent = displayValue;
 }
